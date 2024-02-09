a2558926d836de5d4995ce457455f34eb6da6b02
Jonathan Gontijo <jonathan.ferreira1312@hotmail.com>
2024-02-09 10:31:29 -0300

    first commit

---
+++ lib/common/widgets/custom_Button.dart
@@ -0,0 +1,23 @@
+import 'package:ecommerce/constants/global_variables.dart';
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onTap;
+  const CustomButton({super.key, required this.text, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onTap,
+      child: Text(
+        text,
+        style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+      ),
+      style: ElevatedButton.styleFrom(
+        backgroundColor: GlobalVariables.secondaryColor,
+        minimumSize: const Size(double.infinity, 50),
+      ),
+    );
+  }
+}
---
+++ lib/routes.dart
@@ -0,0 +1,21 @@
+import 'package:ecommerce/features/auth/screens/auth_screen.dart';
+import 'package:flutter/material.dart';
+
+Route<dynamic> generateRoutes(RouteSettings routeSettings) {
+  switch (routeSettings.name) {
+    case AuthScreen.routeName:
+      return MaterialPageRoute(
+        settings: routeSettings,
+        builder: (_) => const AuthScreen(),
+      );
+    default:
+      return MaterialPageRoute(
+        settings: routeSettings,
+        builder: (_) => const Scaffold(
+          body: Center(
+            child: Text('This screen does not exist!'),
+          ),
+        ),
+      );
+  }
+}
